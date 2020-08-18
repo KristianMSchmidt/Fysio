db114b0225e8f6a1bf383aabc66817d476ee830c
KristianMSchmidt <krms@kvuc.dk>
2020-08-18 16:27:47 +0200

    ...

--- pages/frontpage.php
+++ pages/frontpage.php
@@ -18,7 +18,7 @@
         <a data-direction = "reverse" id="pageHeader"
         class="ui-btn ui-corner-all ui-btn-icon-notext ui-icon-bars" href= "#mypanel"></a>   
         
-        <h1 id="headerTitle">Fysio 17</h1>
+        <h1 id="headerTitle"></h1>
         
         <img src = "images/dfys-logo4.gif" id = "logo" class = "ui-btn  ui-corner-all 
         ui-btn-icon-notext ui-btn-right"> 
@@ -28,22 +28,33 @@
  
     <!-- page content -->
     <div role="main" class="ui-content">
-        <h3>Måleredskaber og tests </h3>     
+        <h3>Måleredskaber og test</h3>     
+
+        <div data-role="tabs" id="tabs">
+
+        <div data-role="navbar"><ul><li><a href="#" class="ui-btn-active">Krop & funktion</a></li>
+        <li><a href="#">Specialer</a></li><li><a href="#">ABC</a></li></ul></div><!-- /navbar -->
+
+ 
+
+
         <div id="Krop&funktion" style="display: block">            
             <ul data-role="listview" data-inset="true">
                 <li><a href="#nakke/ryg">Nakke/ryg</a></li>
-                <li><a href="#skulder">Skulders</a></li>
-                <li><a href="#hofte">Hofteg</a></li>
+                <li><a href="#skulder">Skulder</a></li>
+                <li><a href="#hofte">Hofte</a></li>
                 <li><a href="#knæ">Knæ</a></li>
                 <li><a href="#ankel">Ankel</a></li>
                 <li><a href="#balance">Balance</a></li>  
                 <li><a href="#gang">Gang</a></li>  
                 <li><a href="#mobilitet">Mobilitet</a></li>  
-                <li><a href="#funktion">Funktion(niveau/motorisk)</a></li>  
+                <li><a href="#funktion">Funktion (niveau/motorisk)</a></li>  
                 <li><a href="#smerte">Smerte</a></li>  
                 <li><a href="#kondition">Kondition</a></li>  
                 <li><a href="#livskvalitet">Livskvalitet</a></li>  
             </ul>
+        <div>
+    
     </div>
 </div>
     
